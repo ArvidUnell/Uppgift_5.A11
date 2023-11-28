3c8d5bacecf125416b838ad0ce5e88756944238e
ArvidUnell <arun06001@utb.vaxjo.se>
2023-11-28 22:50:53 +0100

    Add project files.

---
+++ Uppgift_5.A11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_5_A11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 61, 51414, -1321, 312345, 61123, -56223, 5601 };
+
+            List<int> inputList = input.ToList();
+
+            while (inputList.Count > input.Length/2 + 1)
+            {
+                inputList.Remove(inputList.Min());
+            }
+
+            Console.WriteLine($"Medianen är {inputList.Min()}");
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
